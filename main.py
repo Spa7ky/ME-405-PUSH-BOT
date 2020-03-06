54b48b5168e56e490b3f6c612591622e6475057f
gwalters132 <gwalters132@gmail.com>
2020-03-05 23:53:28 -0800

    updated code 3/5

--- main.py
+++ main.py
@@ -11,10 +11,12 @@ from VL53L0X import VL53L0X
 from machine import I2C
 import utime
 
-i2c2 = I2C(3,freq = 200000) #Uses bus 3 because of the pins it is connected to
-TOF1 = VL53L0X(i2c2)
-TOF1.start()
-TOF1.read()
+i2c = I2C(1,freq = 200000) #Uses bus 3 because of the pins it is connected to
+i2c2 = I2C(3,freq = 200000)
+TOF1 = VL53L0X(i2c)
+TOF2 = VL53L0X(i2c2)
+#TOF1.start()
+#TOF1.read()
 print('done TOF')
 
 
@@ -22,13 +24,14 @@ print('done TOF')
 MPU 
 '''
 from imu import MPU6050
-i2c = I2C(1,freq = 200000)
+#i2c = I2C(1,freq = 200000)
 imu = MPU6050(i2c)
 #print(imu.accel.xyz)
 
 while True:
     print(imu.accel.xyz)
     TOF1.read()
+    TOF2.read()
     #print(imu.gyro.xyz)
 
     #print(imu.accel.z)
